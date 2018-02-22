5b36842d99c24aac716ad2c04788f0be0a775c1f
Stijn Diependaele <stijn.diependaele@vdab.be>
2018-02-22 16:44:24 +0100

    Add logging

--- review.py
+++ review.py
@@ -4,9 +4,16 @@ import fire
 import subprocess
 import re
 import os
+import logging
 
  
 def cmr(name):
+    logger = logging.getLogger()
+    logger.setLevel(logging.INFO)
+    ch = logging.StreamHandler()
+    ch.setLevel(logging.DEBUG)
+    logger.addHandler(ch)
+
     gitlab_token = os.getenv('GITLAB_TOKEN')
     if gitlab_token is None:
         print("No Gitlab private token found in environment variables, please set one and try again")
@@ -23,8 +30,8 @@ def cmr(name):
     
     hostname = re.sub(r'^.*[@:](.+\..+)[\:].+', r'\1', output).strip()
     
-    print(project_id_url)
-    print(hostname)
+    logger.info('URL safe project name:' + project_id_url)
+    logger.info('Remote hostname' + hostname)
     project_id_int = int(requests.get("http://" + hostname + "/api/v4/projects/" + project_id_url, headers=headers).json()['id'])
     payload = {
             'id': project_id_int,
@@ -33,7 +40,7 @@ def cmr(name):
             'title': name
             }
     req = requests.post('http://' + hostname + '/api/v4/projects/' + project_id_url + '/merge_requests', headers=headers, data=payload)
-    print(req.status_code)
+    logger.info('Response Status:' + req.status_code)
     print(req.text)
 
 if __name__ == "__main__":
